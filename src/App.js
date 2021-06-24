041554752c052b74c2d407afbfdfc19f0141ef55
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-06-24 16:57:53 +0530

    route setup and packages

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Checkout from "./Components/Checkout";
 import Login from "./Components/Login";
+import Payment from "./Components/Payment";
+import Orders from "./Components/Orders";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { useEffect } from "react";
 import { auth } from "./Components/Firebase";
 import { useStateValue } from "./Components/StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51J5pfeEzDeKpALqjugubZyYtcejms8mcemQFWNlf431rS7bnz1Qe6Wh7EoEVSljT7xtcCkIFjRZXCCZdrI46CUSZ00G4IaKKBe"
+);
 
 function App() {
-  const [ {}, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       console.log("THE USER is>>> ", authUser);
 
       if (authUser) {
-
         dispatch({
-          type:'SET_USER',
-          user:authUser
-        })
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
         dispatch({
-          type:'SET_USER',
-          user:null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
     });
   }, []);
@@ -33,12 +41,22 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
